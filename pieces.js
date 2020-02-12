e28b1d9ab6033093c2b1c88b14c3f8886d795377
MilanBrkic <milan.brkic1998@gmail.com>
2020-02-13 00:47:06 +0100

    setting pieces on the board

--- pieces.js
+++ pieces.js
@@ -1,36 +1,113 @@
 class Piece{
-    constructor(color,img){
+    constructor(color){
         this.color = color; //w for white b for black
-        this.img = imgTagGenerate(img);
     }
     
 }
 
-class King extends Piece{  
-    
-    constructor(color,img){
-        super(color,img);
+class King extends Piece{    
+    constructor(color){
+        super(color);
+        
         if(this.color=="w"){
-            this.current = {x:3,y:0}
+            this.img = imgTagGenerate('king_white.png');
+            this.current = {x:3,y:7}
         }
         else{
-            this.current =  {x:3,y:7};
+            this.img = imgTagGenerate('king_black.png');
+            this.current =  {x:3,y:0};
         }
         
     }
-    startingPosition(){
-        if(this.color=="w"){
-            return {x:3,y:0};
-        }
-        else{
-            return {x:3,y:7};
-        }
-    }
-    
+
     setCurrent(coor){
         this.current = coor;
     }
     
+}
+
+class Queen extends Piece{    
+    constructor(color){
+        super(color);
+        if(this.color=="w"){
+            this.img = imgTagGenerate('queen_white.png');
+            this.current = {x:4,y:7}
+        }
+        else{
+            this.img = imgTagGenerate('queen_black.png');
+            this.current =  {x:4,y:0};
+        }
+        
+    }
+
+    setCurrent(coor){
+        this.current = coor;
+    }
+    
+}
+
+class Bishop extends Piece{
+    constructor(color,current){
+        super(color);
+        this.current = current;
+        if(this.color=="w"){
+            this.img = imgTagGenerate('bishop_white.png');
+        }
+        else{
+            this.img = imgTagGenerate('bishop_black.png');
+        }
+    }
+    setCurrent(coor){
+        this.current = coor;
+    }
+}
+
+class Knight extends Piece{
+    constructor(color,current){
+        super(color);
+        this.current = current;
+        if(this.color=="w"){
+            this.img = imgTagGenerate('knight_white.png');
+        }
+        else{
+            this.img = imgTagGenerate('knight_black.png');
+        }
+    }
+    setCurrent(coor){
+        this.current = coor;
+    }
+}
+
+class Rook extends Piece{
+    constructor(color,current){
+        super(color);
+        this.current = current;
+        if(this.color=="w"){
+            this.img = imgTagGenerate('rook_white.png');
+        }
+        else{
+            this.img = imgTagGenerate('rook_black.png');
+        }
+    }
+    setCurrent(coor){
+        this.current = coor;
+    }
+}
+
+class Pawn extends Piece{
+    constructor(color,current){
+        super(color);
+        this.current = current;
+        if(this.color=="w"){
+            this.img = imgTagGenerate('pawn_white.png');
+        }
+        else{
+            this.img = imgTagGenerate('pawn_black.png');
+        }
+    }
+    setCurrent(coor){
+        this.current = coor;
+    }
 }
 
 function imgTagGenerate(imgName){
--- script.js
+++ script.js
@@ -3,17 +3,28 @@ var matrix = new Array();
 var pieces = new Array();
 
 
-king_white = new King("w", "king_white.png");
-king_black = new King("b", "king_black.png");
+var king_white;
+var king_black;
 
-king_white.setCurrent({x:2,y:4});
-console.log(king_black.current,king_white.current);
+var queen_white;
+var queen_black;
 
+var bishop_white;
+var bishop_black;
+
+var knight_white;
+var knight_black;
+
+var rook_white;
+var rook_black;
+
+var pawn_black;
+var pawn_white;
 
 addTable();
+setPieces();    
 
 
-matrix[king_white.startingPosition().y][king_white.startingPosition().x].innerHTML = king_white.img;
 
 function addTable(){
     for(let j = 0;j<8;j++){
@@ -29,5 +40,49 @@ function addTable(){
     
 }
 
+function initializePieces(){
+    king_white = new King("w");
+    king_black = new King("b");
+
+    queen_white = new Queen("w");
+    queen_black = new Queen("b");
+
+    bishop_black = new Array(new Bishop("b",{x:2,y:0}), new Bishop("b",{x:5,y:0}));
+    bishop_white = new Array(new Bishop("w",{x:2,y:7}), new Bishop("w",{x:5,y:7}));
+
+    knight_black = new Array(new Knight("b",{x:1,y:0}), new Knight("b",{x:6,y:0}));
+    knight_white = new Array(new Knight("w",{x:1,y:7}), new Knight("w",{x:6,y:7}));
+
+    rook_black = new Array(new Rook("b",{x:0,y:0}), new Rook("b",{x:7,y:0}));
+    rook_white = new Array(new Rook("w",{x:0,y:7}), new Rook("w",{x:7,y:7}));
+
+    pawn_black = new Array();
+    pawn_white = new Array();
+
+    for(let i=0;i<8;i++){
+        pawn_black.push(new Pawn("b", {x:i,y:1}));
+        pawn_white.push(new Pawn("w", {x:i,y:6}));
+    }
+
+}
+
+function setPieces(){
+    initializePieces();
+
+    arr = new Array(king_black,king_white,queen_black,queen_white);
+
+    for(let i = 0;i<2;i++){
+        arr.push(bishop_black[i],bishop_white[i], knight_black[i],knight_white[i], rook_black[i], rook_white[i]);
+    }
+
+    for(i =0;i<8;i++){
+        arr.push(pawn_black[i],pawn_white[i]);
+    }
+
+    for(i = 0;i<arr.length;i++){
+        let element = arr[i];
+        matrix[element.current.y][element.current.x].innerHTML = element.img;
+    }
+}
 
 
